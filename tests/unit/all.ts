fdbfc9b82b7706c08715624e2b2a08ee3a89f7dc
Kitson Kelly <me@kitsonkelly.com>
2017-03-31 17:58:15 +0100

    Minor cleanups

--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,8 +1,3 @@
 import './ClientErrorCollector';
 import './main';
-<<<<<<< HEAD
-=======
-import './dom/all';
 import './support/all';
-import './widgets/all';
->>>>>>> 91f885a... WIP: assertRender
