33d010b15af0e42023c653ddcc3e2fce6eedb8e5
Kitson Kelly <me@kitsonkelly.com>
2017-04-01 12:38:42 +0100

    Merge featutre-assert-render

--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,3 +1,5 @@
+import 'intern/dojo/has!host-node?../../src/support/loadJsdom';
 import './ClientErrorCollector';
+import './harness';
 import './main';
 import './support/all';
--- tests/unit/support/all.ts
+++ tests/unit/support/all.ts
@@ -1,3 +1,5 @@
 import './compare';
 import './AssertionError';
 import './assertRender';
+import './dispatchEvent';
+import 'intern/dojo/has!host-node?./loadJsdom';
